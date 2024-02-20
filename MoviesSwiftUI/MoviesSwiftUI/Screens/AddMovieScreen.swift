3db4e383c05096c85bee1c60e403a7c13c7463f7
Jason <sanchezjason@me.com>
2024-02-19 19:03:03 -0700

    swiftui/macros/movies- @State, @Enviroment, NavigationLink

--- MoviesSwiftUI/MoviesSwiftUI/Screens/AddMovieScreen.swift
+++ MoviesSwiftUI/MoviesSwiftUI/Screens/AddMovieScreen.swift
@@ -6,20 +6,49 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct AddMovieScreen: View {
+    @Environment(\.dismiss) private var dismiss
+    @Environment(\.modelContext) private var context
     
     @State private var title: String = ""
     @State private var year: Int?
+    
+    private var isFormValid: Bool {
+        !title.isEmptyOrWhiteSpace && year != nil
+    }
     
     var body: some View {
         Form {
             TextField("Title", text: $title)
             TextField("Year", value: $year, format: .number)
         }
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button("Close") {
+                    //dismiss()
+                }
+            }
+            ToolbarItem(placement: .topBarTrailing) {
+                Button("Save") {
+                    guard let year = year else { return }
+                    let movie = Movie(title: title, year: year)
+                    context.insert(movie)
+                    do {
+                        try context.save()
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+                }.disabled(!isFormValid)
+            }
+        }
     }
 }
 
 #Preview {
-    AddMovieScreen()
+    NavigationStack {
+        AddMovieScreen()
+            .modelContainer(for: [Movie.self])
+    }
 }
